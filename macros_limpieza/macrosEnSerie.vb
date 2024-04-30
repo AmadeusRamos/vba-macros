c136dd48e07b84f84a88d42a5c316ec0747dbd8a
Amadeus <emmani.ramos@gmail.com>
2024-04-30 17:36:33 -0600

    Creación de archivo macrosEnSerie
    Se crea el archivo de macrosEnSerie.vb y también se editan los archivos de pruebas.vb y macroTotale.vb

---
+++ macros_limpieza/macrosEnSerie.vb
@@ -0,0 +1,34 @@
+Sub cadenaMacros()
+
+    eliminarND
+    cartoStrack
+
+End Sub
+
+Sub serieMacros()
+
+    eliminarLibroStrack
+    moverLibroStrack
+    abrirLibroStrack
+    cambiarNombreHoja
+    eliminarND
+    cartoStrack
+    abrirLibroLimpieza
+    limpiarRango
+    copiarStrackLimpieza
+    copiarFormulas
+    cerrarLibroStrack
+    copiarDai2
+    suprimirDuplicadosND
+    limpiarHojas
+    ordenaPorMunicipio
+    filasDAI
+    filasSeguridad
+    filasAccidentes
+    abrirLibroGeneral
+    limpiarLibroGeneral
+    deLimpieza_a_General
+    cerrarLibroLimpieza
+    cerrarLibroGeneral
+
+End Sub
