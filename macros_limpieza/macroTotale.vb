78e0806c717cd34883b76681946da2578852368f
Amadeus <emmani.ramos@gmail.com>
2024-04-30 17:33:09 -0600

    Actualización de macroTotale y pruebas

--- macros_limpieza/macroTotale.vb
+++ macros_limpieza/macroTotale.vb
@@ -516,3 +516,31 @@ Application.ScreenUpdating = False
 Application.ScreenUpdating = True
 
 End Sub
+
+Sub serieMacros()
+
+    eliminarLibroStrack
+    moverLibroStrack
+    abrirLibroStrack
+    cambiarNombreHoja
+    eliminarND
+    cartoStrack
+    abrirLibroLimpieza
+    limpiarRango
+    copiarStrackLimpieza
+    copiarFormulas
+    cerrarLibroStrack
+    copiarDai2
+    suprimirDuplicadosND
+    limpiarHojas
+    ordenaPorMunicipio
+    filasDAI
+    filasSeguridad
+    filasAccidentes
+    abrirLibroGeneral
+    limpiarLibroGeneral
+    deLimpieza_a_General
+    cerrarLibroLimpieza
+    cerrarLibroGeneral
+
+End Sub
--- pruebas.vb
+++ pruebas.vb
@@ -267,34 +267,6 @@ Sub cadenaMacros()
 
 End Sub
 
-Sub serieMacros()
-
-    eliminarLibroStrack
-    moverLibroStrack
-    abrirLibroStrack
-    cambiarNombreHoja
-    eliminarND
-    cartoStrack
-    abrirLibroLimpieza
-    limpiarRango
-    copiarStrackLimpieza
-    copiarFormulas
-    cerrarLibroStrack
-    copiarDai2
-    suprimirDuplicadosND
-    limpiarHojas
-    ordenaPorMunicipio
-    filasDAI
-    filasSeguridad
-    filasAccidentes
-    abrirLibroGeneral
-    limpiarLibroGeneral
-    deLimpieza_a_General
-    cerrarLibroLimpieza
-    cerrarLibroGeneral
-
-End Sub
-
 Sub cambiarSeleccion()
 
 
